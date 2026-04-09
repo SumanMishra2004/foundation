623ead12d92e8ebbab1009978ff53bbef56b7511
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-04-08 23:06:58 -0400

    Admin opted out

--- components/layout/TrusteePartner.tsx
+++ components/layout/TrusteePartner.tsx
@@ -2,26 +2,37 @@ import "../../app/globals.css";
 import Image from "next/image";
 import TrusteePic from "@/public/trustee.jpg";
 const TrusteePartner = () => {
-
   return (
-    <div className="container max-w-7xl mx-auto bg-yellow-400 overflow-hidden">
-      
-      <div className="flex gap-5 py-5 animate-scroll whitespace-nowrap">
-        {Array.from({ length: 10 }).map((_, index) => {
+    <div className="container max-w-7xl mx-auto bg-gray-200">
+      <div className="overflow-hidden px-5 group">
+      <div className="flex gap-5 py-5  animate-scroll group-hover:[animation-play-state:paused]">
+        {[...Array.from({ length: 15})].map((_, index) => {
           return (
-            <div key={index} className="rounded-full">
+            <div key={index} className=" rounded-full ">
               <Image
-                src={TrusteePic}
+                src={`https://i.pravatar.cc/150?img=${index + 1}`}
                 alt="Trustee/Partner"
                 width={60}
                 height={60}
-                className="rounded-full"
+                className="rounded-full hover:scale-110 transition-transform duration-300 ease-in"
               />
             </div>
           );
         })}
-   
-    
+             {[...Array.from({ length: 15})].map((_, index) => {
+          return (
+            <div key={index} className="rounded-full shrink-0 grow-0 aria-hidden">
+              <Image
+                src={`https://i.pravatar.cc/150?img=${index + 1}`}
+                alt="Trustee/Partner"
+                width={60}
+                height={60}
+                className="rounded-full hover:scale-110 transition-transform duration-300 ease-in"
+              />
+            </div>
+          );
+        })}
+      </div>
       </div>
     </div>
   );
